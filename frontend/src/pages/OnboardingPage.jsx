031f73c5d18ae3c69088b113a0863df1c7bf2917
Hema-733 <hemavarshanar06@gmail.com>
2026-08-17 22:49:25 +0530

    setup frontend React application

---
+++ frontend/src/pages/OnboardingPage.jsx
@@ -0,0 +1,10 @@
+function OnboardingPage() {
+  return (
+    <div>
+      <h1>Employee Onboarding</h1>
+      <p>Onboarding form will be added here.</p>
+    </div>
+  );
+}
+
+export default OnboardingPage;
